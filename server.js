e82328a48db67fecc9ac6a0c573c96d594d1ffa9
MercylessReap <mercilessreap@gmail.com>
2018-05-22 22:28:21 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,30 +1,52 @@
 const express = require('express')
-    , bodyParser = require('body-parser')
+    , cookieParser = require('cookie-parser')
+    , expressSession = require('express-session')
+    , methodOverride = require('method-override')
+    , passport = require('passport')
     , path = require('path')
-      global.rootDir = path.resolve(__dirname);
-      //test for locking down app
-      global.user = {id:'5ab908dee7084e3638e36198', name:'Swaye Chateau',department:'5abea0315abfbb0b50afdc0e',perm:{admin:false, wizard:true}}
-    , index = require('./routes/webserver')
-    , api = require('./routes/api')
-    , bot = require('./routes/bot')
+      global.rootDir = path.resolve(__dirname)
+      require('./lib/auth/passport')
+      global.ensureAuthenticated = function(req, res, next) {
+        if (req.isAuthenticated()) { return next(); }
+          res.redirect('/auth');
+      }
+      global.user = {id:'5afcce86fb860e4dcc59a977', name:'Swaye Chateau', perm:{admin:false, wizard:true,department:'5abea0315abfbb0b50afdc0e'}}
+    , app = express()
     , port = process.env.PORT || 80
-    , app = express();
+    , util = require('util')
+    , bunyan = require('bunyan')
+    , bodyParser = require('body-parser')
+    , routes = require('./routes/index')
+    , morgan = require('morgan')
 
-    
+    /*, azureConfig = require(rootDir+'/lib/auth/azure')
+    , passport = require('passport')
+    , OIDCStrategy = require('passport-azure-ad').OIDCStrategy;*/
+    var log = bunyan.createLogger({
+        name: 'Microsoft OIDC Bot Framework Application'
+    });
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev')); // log every request to the console
 
+app.use(methodOverride());
+app.use(cookieParser());
+
+app.use(expressSession({ secret: 'keyboard cat', resave: true, saveUninitialized: false }));
+
+app.use(bodyParser.json());  // get information from html forms
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+// required for passport
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
-app.use('/', index);
-app.use('/api', api);
-app.use('/bot', bot);
+app.use(express.static(__dirname + '/public'));
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')))
+app.use('/', routes);
 
-// define the 404 page route (leave last)
-app.get('*', (req, res)=>{res.status(404).render('pages/error');});
 
-app.listen(port, () =>{console.log("Server running on port:" + port);})
+
+app.listen(port)
